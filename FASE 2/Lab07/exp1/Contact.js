fa99e781e239611702d0548274f44feef5a81490
FabricioMiotaT <142760179+FabricioMiotaT@users.noreply.github.com>
2023-10-15 20:43:23 -0500

    Experiencia 01 y 02 - Lab 07

---
+++ FASE 2/Lab07/exp1/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contacto</h1>
+      <p>este es el apartado de contacto</p>
+    </div>
+  );
+}
+
+export default Contact;
